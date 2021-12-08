fb812f27004f4303d3fa2d1d93fd55fe3690db13
Bharti Yadav <yada0022@algonquinlive.com>
2021-12-08 08:14:55 -0500

    changes

--- EmilyD_BhartiY/sampling_freq.py
+++ EmilyD_BhartiY/sampling_freq.py
@@ -1,3 +1,4 @@
+import json
 import water_stn_converter as wsc
 import csv
 
@@ -14,7 +15,10 @@ def create_sampling_report(in_json_filename, out_csv_filename):
 
 # with open(in_json_filename) as infile
 #Hi Emily  since we are opening the json file here, should we be using csv.writer?
-#
+#with open(in_json_filename) as json_file:
+#     data = json.load(json_file)
+#We also have a function in water_stn_converter that converts the json file into csv
+#Should we call it here
 # 
 # with open(out_csv_filename,'w') as outfile:
 #      outfile.write(wsc.get_sampling_frequencies())            
